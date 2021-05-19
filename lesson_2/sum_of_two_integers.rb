0f7e3b1da73c66f08fa5b6b4d5692fed72243ff6
riazmohamed <railsdevriaz@gmail.com>
2021-05-19 12:19:14 +0100

    sum_of_two_integers.rb

---
+++ lesson_2/sum_of_two_integers.rb
@@ -0,0 +1,31 @@
+# sum_of_two_integers.rb
+
+# write a pseudocode for a method that returns the sum of two integers
+
+=begin
+keywords: START, SET, GET, PRINT, READ, IF, ELSE IF, ELSE, WHILE, END
+
+START
+
+# given two integers
+
+SET integer1 = the first given integer
+SET integer2 = the second given integer
+
+SET sum_of_two_integers = integer1 + integer2
+
+PRINT sum_of_two_integers
+
+END
+
+=end
+
+def add(a, b)
+  num1 = a
+  num2 = b
+
+  sum = num1 + num2
+end
+
+
+p add(1, 3)
