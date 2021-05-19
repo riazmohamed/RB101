d629671e64f733809cffa13b9c7d20ec35f6c564
riazmohamed <railsdevriaz@gmail.com>
2021-05-19 12:12:24 +0100

    add pseudo code

--- lesson_2/pseudo_code.rb
+++ lesson_2/pseudo_code.rb
@@ -1,3 +1,42 @@
+# pseudo code
+=begin
+  ------- Informal
+  Given a collection of integers
+
+  Iterate through the collection one by one
+  - save the first value as a starting value
+  - for each iteration compare the saved value with the current value
+  - if the saved value is greater, or it is the same
+    - move to the next value in collection
+  - otherwise, if the current value is greater
+    - reassign the saved value as the current value
+
+  After iterating through the collection, return the saved value
+
+
+  ---------- Formal Pseudo code
+  START
+
+  # Given a collection of integers called "numbers"
+
+  SET iterator = 1
+  SET saved_number = value within the collection at the space 1
+
+  WHILE iterator <= length of the numbers
+    SET current_number = value within the collection at the space "iterator"
+    IF saved_number >= current_number
+      go to the next iteration
+    ELSE
+      saved_number = current_number
+
+    iterator = iterator += 1
+
+  PRINT saved_number
+  
+  END
+=end
+
+
 def find_greatest(numbers)
   saved_number = nil
 
