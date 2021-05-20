e985cc229cab975d301e7e3bb21012c054b96807
riazmohamed <railsdevriaz@gmail.com>
2021-05-20 11:29:07 +0100

    pseudo code practice for calculator.rb

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -5,6 +5,51 @@
 # ask the user for an operation to perform
 # output the result
 
+# MORE PSEUDOCODE
+
+=begin
+keywords: START, SET, GET, PRINT, READ, IF, ELSE IF, ELSE, WHILE, END, SUBPROCESS
+
+========== casual pseudo code ==========
+
+while ther user wants to keep going
+  - ask the user for a collection of numbers
+  - iterate through the collection one by one
+    - save the first value as the starting value
+    - for iteration compare the saved value with the current value
+    - if the second value is greater, or it is the save_game
+      - move on to the next value in the collection
+    - otherwise, if the current value is greater
+      - reassign the saved value as the current value
+
+  - after iterating through the collection asve the largest value into the list
+  - ask the user if they want to input another collection
+
+return the saved list of numbers
+
+========== formal pseudo code ==========
+
+START
+
+SET large_numbers = []
+SET keep_going = true
+
+WHILE keep_going == true
+  GET "enter a collection"
+  SET collection
+  SET largest_number = SUBPROCESS "extract the largest number from that collection"
+  large_numbers.push(largest_number)
+  GET "enter another collection?"
+  IF "yes"
+    keep_going == true
+  ELSE
+    keep_going = false
+  IF keep_going == false
+    exit the loop
+
+PRINT large_numbers
+=end
+
 Kernel.puts("Welcome to the Calculator!")
 
 Kernel.puts("Please enter the first number")
