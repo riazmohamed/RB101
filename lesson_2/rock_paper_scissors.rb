2a7892174e216cc5d05adcbaf9a889edbbf4e05d
riazmohamed <railsdevriaz@gmail.com>
2021-06-01 22:40:32 +0100

    add logic for the game

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -5,18 +5,38 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-choice = ''
 loop do
-  prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-  choice = Kernel.gets().chomp()
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp()
 
-  if VALID_CHOICES.include?(choice)
-    break
-  else
-    prompt("That's not a valid choice.")
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
   end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+  if (choice == 'rock' && computer_choice == 'scissors') ||
+      (choice == 'paper' && computer_choice == 'rock') ||
+      (choice == 'scissors' && computer_choice == 'paper')
+    prompt("You win!")
+  elsif (choice == 'rock' && computer_choice == 'paper') ||
+        (choice == 'paper' && computer_choice == 'scissors') ||
+        (choice == 'scissors' && computer_choice == 'rock')
+    prompt("Computer won!")
+  else
+    prompt("it's a tie!")
+  end
+
+  prompt("Do you want to play again?")
+  answer = Kernel.gets().chomp
+  break unless answer.downcase().start_with?('y')
 end
 
-computer_choice = VALID_CHOICES.sample
-
-Kernel.puts("You chose: #{choice}; Computer chose: #{computer_choice}")
+prompt("Thank you for playing Good bye!")
