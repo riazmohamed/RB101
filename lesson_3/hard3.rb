e799fecc13988b25b559d7fba0cc182063a6ec3d
riazmohamed <railsdevriaz@gmail.com>
2021-06-07 21:23:26 +0100

    add hard problems qn 3

---
+++ lesson_3/hard3.rb
@@ -0,0 +1,49 @@
+def mess_with_vars(one, two, three)
+  one = two         # re-assignment hence the local variable (one) points to a different object
+  two = three
+  three = one
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}" # "one is: one"
+puts "two is: #{two}" # "two is: two"
+puts "three is: #{three}" # "three is: three"
+
+puts "================ b)"
+def mess_with_vars(one, two, three)
+  one = "two"
+  two = "three"
+  three = "one"
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+
+puts "================ c)"
+def mess_with_vars(one, two, three)
+  one.gsub!("one","two")
+  two.gsub!("two","three")
+  three.gsub!("three","one")
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
