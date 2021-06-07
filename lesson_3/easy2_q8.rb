715c3fcd4d1dda33e743dadebd8869907de7af4a
riazmohamed <railsdevriaz@gmail.com>
2021-06-07 11:27:14 +0100

    add easy2 q9

--- lesson_3/easy2_q8.rb
+++ lesson_3/easy2_q8.rb
@@ -2,3 +2,10 @@ advice = "Few things in life are as important as house training your pet dinosau
 
 p advice.slice!("Few things in life are as important as ")
 p advice
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+p advice.slice!(0, advice.index('house'))
+p advice
+
+# String#slice does not mutate the caller
