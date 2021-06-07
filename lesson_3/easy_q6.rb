fdc6b209a6a5410c841432adaedc136ae2ecfa3f
riazmohamed <railsdevriaz@gmail.com>
2021-06-07 10:10:31 +0100

    add easy q6

---
+++ lesson_3/easy_q6.rb
@@ -0,0 +1,13 @@
+# two different ways to put the expected "Four score and " in front of it.
+
+famous_words = "seven years ago..."
+famous_words.prepend("Four score and ")
+p famous_words
+
+famous_words = "seven years ago..."
+famous_words = "Four score and " + famous_words
+p famous_words
+
+famous_words = "seven years ago..."
+famous_words = "Four score and " << famous_words
+p famous_words
