90110a32492928d177121c92c913ade641e162b2
riazmohamed <railsdevriaz@gmail.com>
2021-06-07 10:20:15 +0100

    easy q8

---
+++ lesson_3/easy_q8.rb
@@ -0,0 +1,9 @@
+# Turn this into an array containing only two elements: Barney's name and Barney's number
+
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+p flintstones.select { |k, v| k == "Barney" }.to_a.flatten!
+
+puts "\t========option 2========"
+
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+p flintstones.assoc("Barney")
