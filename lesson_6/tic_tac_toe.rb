6db9ccaa49506e01207f8ef81b4eca5c954be8c9
riazmohamed <railsdevriaz@gmail.com>
2021-06-16 17:22:31 +0100

    add Player Input

--- lesson_6/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe.rb
@@ -1,7 +1,14 @@
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
 def display_board(brd)
   puts ""
-  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
   puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
   puts "     |     |"
   puts "-----+-----+-----"
   puts "     |     |"
@@ -16,9 +23,30 @@ end
 
 def initialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = ' ' }
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def player_places_pieces(brd)
+  square = ''
+  loop do
+    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
+    square = gets.chomp.to_i
+
+    break if empty_squares(brd).include?(square)
+    prompt "Sorry, thats not a valid choice!"
+
+  end
+  brd[square] = PLAYER_MARKER
+end
+
 board = initialize_board
+display_board(board)
+
+player_places_pieces(board)
+puts board.inspect
 display_board(board)
