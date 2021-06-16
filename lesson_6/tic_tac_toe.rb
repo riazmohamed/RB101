ffd534fa2593a9ca90949962280ed70f4556147b
riazmohamed <railsdevriaz@gmail.com>
2021-06-16 22:19:53 +0100

    add logic for Play again?

--- lesson_6/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe.rb
@@ -10,6 +10,7 @@ end
 
 def display_board(brd)
   system 'clear'
+  puts "You're a #{PLAYER_MARKER}. Computer is a #{COMPUTER_MARKER}"
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -80,19 +81,27 @@ def detect_winner(brd)
   nil
 end
 
-board = initialize_board
-
 loop do
-  display_board(board)
-  player_places_piece!(board)
-  break if someone_won?(board) || board_full?(board)
-  computer_places_piece!(board)
-  break if someone_won?(board) || board_full?(board)
-end
-display_board(board)
+  board = initialize_board
 
-if someone_won?(board)
-  prompt "#{detect_winner(board)} won!"
-else
-  prompt "It's a tie!"
+  loop do
+    display_board(board)
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+  display_board(board)
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end
+
+  prompt "Play again? (y or n)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
+
+prompt "Thanks for playing Tic Tac Toe. GoodBye!"
