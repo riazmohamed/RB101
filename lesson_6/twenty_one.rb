fdcc3eb51f3888a507bc8a2573db97aa98f47719
riazmohamed <railsdevriaz@gmail.com>
2021-06-24 19:51:55 +0100

    refractor player_total and dealer_total

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -36,9 +36,7 @@ def busted?(cards)
   total(cards) > 21
 end
 
-def detect_result(dealer_cards, player_cards)
-  player_total = total(player_cards)
-  dealer_total = total(dealer_cards)
+def detect_result(dealer_total, player_total)
 
   if player_total > 21
     :player_busted
@@ -53,8 +51,8 @@ def detect_result(dealer_cards, player_cards)
   end
 end
 
-def display_result(dealer_cards, player_cards)
-  result = detect_result(dealer_cards, player_cards)
+def display_result(dealer_total, player_total)
+  result = detect_result(dealer_total, player_total)
 
   case result
   when :player_busted
@@ -89,9 +87,12 @@ loop do
     dealer_cards << deck.pop
   end
 
+  player_total = total(player_cards)
+  dealer_total = total(dealer_cards)
+
   prompt "Dealer has #{dealer_cards[0]} and ?"
   # rubocop:disable Layout/LineLength
-  prompt "You have: #{player_cards[0]} and #{player_cards[1]}, for a total of #{total(player_cards)}."
+  prompt "You have: #{player_cards[0]} and #{player_cards[1]}, for a total of #{player_total}."
   # rubocop:enable Layout/LineLength
 
   loop do
@@ -107,42 +108,44 @@ loop do
       player_cards << deck.pop
       prompt "You chose to hit!"
       prompt "You cards are now: #{player_cards}"
-      prompt "Your total is now: #{total(player_cards)}"
+      player_total = total(player_cards)
+      prompt "Your total is now: #{player_total}"
     end
 
     break if player_turn == 's' || busted?(player_cards)
   end
 
   if busted?(player_cards)
-    display_result(dealer_cards, player_cards)
+    display_result(dealer_total, player_total)
     play_again? ? next : break
   else
-    prompt "Dealer stays at #{total(dealer_cards)}"
+    prompt "Dealer stays at #{dealer_total}"
   end
 
   prompt "Dealer turn..."
 
   loop do
-    break if total(dealer_cards) >= 17
+    break if dealer_total >= 17
 
     prompt "Dealer hits!"
     dealer_cards << deck.pop
+    dealer_total = total(dealer_cards)
     prompt "Dealer's cards are now: #{dealer_cards}"
   end
 
   if busted?(dealer_cards)
-    prompt "Dealer total is now: #{total(dealer_cards)}"
-    display_result(dealer_cards, player_cards)
+    prompt "Dealer total is now: #{dealer_total}"
+    display_result(dealer_total, player_total)
     play_again? ? next : break
   else
-    prompt "Dealer stays at #{total(dealer_cards)}"
+    prompt "Dealer stays at #{dealer_total}"
   end
 
   puts "==============="
-  prompt "Dealer has #{dealer_cards}, for a total of: #{total(dealer_cards)}"
-  prompt "Player has #{player_cards}, for a total of: #{total(player_cards)}"
+  prompt "Dealer has #{dealer_cards}, for a total of: #{dealer_total}"
+  prompt "Player has #{player_cards}, for a total of: #{player_total}"
 
-  display_result(dealer_cards, player_cards)
+  display_result(dealer_total, player_total)
 
   break unless play_again?
 end
